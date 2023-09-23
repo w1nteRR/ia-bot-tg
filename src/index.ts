04e88ea305fa50c4774755e93378b13f89f871eb
w1nteRR <mykola796@gmail.com>
2023-09-23 14:34:53 +0300

    refactor index

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,19 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 
 import { Bot } from "./bot";
+import { AppConfig } from "./env";
 
-dotenv.config();
-
-const environment = process.env.NODE_ENV || "development";
-dotenv.config({ path: `.env.${environment}` });
-
-const WEB_URL = process.env.WEB_URL;
-const PORT = process.env.PORT;
-const TOKEN = process.env.TOKEN;
+const config = new AppConfig();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const bot = new Bot(TOKEN as string);
-bot.initializeWebApp(WEB_URL as string);
+const bot = new Bot(config.telegramToken);
+bot.initializeWebApp(config.webUrl);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+app.listen(config.port, () => {
+  console.log(`App listening on port ${config.port}`);
 });
